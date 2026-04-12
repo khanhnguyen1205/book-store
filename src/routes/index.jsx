f179527b5590feb16b38675a552edcd147e22b49
khanhnguyen1205 <nvkhanh1252005@gmail.com>
2026-04-12 13:23:17 +0700

    fix lỗi chuyển hướng sang trang đăng kí tài khoản

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,12 +2,14 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import BookList from '../features/book/BookList';
 import BookDetail from '../features/book/BookDetail';
 import Login from '../features/auth/Login';
+import Register from '../features/auth/Register';
 import ProtectedRoute from './ProtectedRoute';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
             <Route path="/" element={<BookList />} />
             <Route path="/book/:id" element={<BookDetail />} />
             <Route path="*" element={<Navigate to="/" replace />} />
